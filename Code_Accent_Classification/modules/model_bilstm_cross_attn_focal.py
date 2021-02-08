95a65b65110cb20fe003553a4acca6684fafe8c4
Manav Kaushik <31390994+manavkaushik@users.noreply.github.com>
2021-02-09 01:15:52 +0530

    Update model_bilstm_cross_attn_focal.py

--- Code_Accent_Classification/modules/model_bilstm_cross_attn_focal.py
+++ Code_Accent_Classification/modules/model_bilstm_cross_attn_focal.py
@@ -73,12 +73,12 @@ def bilstm_cross_attn_focal_model():
 
     model.compile(loss=focal_loss(gamma=5., alpha=0.5), optimizer='adam', metrics=['accuracy'])
 
-    es = EarlyStopping(monitor='val_loss', mode='min', verbose=5, patience=4)
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=5, patience=20)
     mc = ModelCheckpoint('../../bilstm_focal_model_wts.h5', monitor='val_loss', mode='min', save_best_only=True, save_weights = True, verbose=1)
     #tb_callback = tf.keras.callbacks.TensorBoard('./logs', update_freq = 1)
         
 
-    hist = model.fit(ds_series_train_batch, validation_data = ds_series_val_batch, epochs=1, callbacks=[es, mc])
+    hist = model.fit(ds_series_train_batch, validation_data = ds_series_val_batch, epochs=100, callbacks=[es, mc])
 
     ############################ TESTING THE MODEL ######################################
 
